9ad0ddfe75d889c004008b74d27ef79e2d1f7916
Vaishak Rajashekar <vaishak.r@dikshatech.com>
2024-08-21 17:38:27 +0530

    Magneto final commit

--- cypress.config.js
+++ cypress.config.js
@@ -4,6 +4,20 @@ const { faker } = require('@faker-js/faker');
 const cucumber = require('cypress-cucumber-preprocessor').default;
 
 module.exports = defineConfig({
+  chromeWebSecurity: false,
+    experimentalSourceRewriting: false,
+    animationDistanceThreshold: 50,
+    watchForFilesChanges: false,
+    viewportWidth: 1980,
+    viewportHeight: 1080,
+    waitForAnimation: true,
+    animationDistanceTreshold: 20,
+    defaultCommandTimeout: 6000,
+    waitForAnimations: false,
+    execTimeout: 600000,
+    pageLoadTimeout: 600000,
+    responseTimeout: 150000,
+    video: false,
   e2e: {
     setupNodeEvents(on, config) {
       on('file:preprocessor', cucumber());
